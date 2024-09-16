132853831e7c6355f4ccb7368ba75dee6b4c8f6f
YuriVGR <yurivgr@icloud.com>
2024-09-16 04:49:41 -0300

    switch component created

---
+++ src/components/switch/index.tsx
@@ -0,0 +1,77 @@
+import {
+  TouchableWithoutFeedback,
+  View,
+  StyleSheet,
+  Animated,
+  LayoutChangeEvent,
+} from "react-native";
+import { useState, useEffect, useRef } from "react";
+import colors from "../../styles/colors";
+
+export default function Switch() {
+  const [isEnabled, setIsEnabled] = useState(false);
+  const [containerWidth, setContainerWidth] = useState(0);
+  const toggleAnim = useRef(new Animated.Value(0)).current;
+
+  const toggleSwitch = () => setIsEnabled((previousState) => !previousState);
+
+  const onLayout = (event: LayoutChangeEvent) => {
+    const { width } = event.nativeEvent.layout;
+    setContainerWidth(width);
+  };
+
+  useEffect(() => {
+    Animated.spring(toggleAnim, {
+      toValue: isEnabled ? 1 : 0,
+      useNativeDriver: false,
+    }).start();
+  }, [isEnabled]);
+
+  const toggleSize = containerWidth * 0.4;
+  const moveDistance = containerWidth - toggleSize - 10;
+
+  return (
+    <TouchableWithoutFeedback onPress={toggleSwitch}>
+      <View
+        style={[styles.container, isEnabled && styles.containerEnabled]}
+        onLayout={onLayout}
+      >
+        <Animated.View
+          style={[
+            styles.toggle,
+            {
+              width: toggleSize,
+              height: toggleSize,
+              transform: [
+                {
+                  translateX: toggleAnim.interpolate({
+                    inputRange: [0, 1],
+                    outputRange: [0, moveDistance],
+                  }),
+                },
+              ],
+            },
+          ]}
+        />
+      </View>
+    </TouchableWithoutFeedback>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: 60,
+    height: 30,
+    backgroundColor: colors.light.textLight,
+    borderRadius: 15,
+    justifyContent: "center",
+    padding: 5,
+  },
+  containerEnabled: {
+    backgroundColor: "#4cd964",
+  },
+  toggle: {
+    backgroundColor: "white",
+    borderRadius: 16,
+  },
+});
