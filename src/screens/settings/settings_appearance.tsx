75fd70675724a8663e595c107080e0d59798ce2e
YuriVGR <yurivgr@icloud.com>
2024-09-18 08:17:39 -0300

    ok ok

---
+++ src/screens/settings/settings_appearance.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+export default function ThemeSettings() {
+  return (
+    <View>
+      <Text>Theme Settings</Text>
+    </View>
+  );
+}
