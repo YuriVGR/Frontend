6b441cb2f188c51d641a044a130fafcda72f1524
Yuri VGR <yurivrg@icloud.com>
2024-09-18 10:02:57 -0300

    Removed ThemeContext for now, will fix it at a later time

--- App.tsx
+++ App.tsx
@@ -2,16 +2,13 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, useColorScheme } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import StackNavigator from "./src/routes/stack.routes";
-import { ThemeProvider } from "./src/context/ThemeContext";
 
 export default function App() {
   return (
-    <ThemeProvider>
-      <NavigationContainer>
-        <StackNavigator />
-        <StatusBar style="auto" />
-      </NavigationContainer>
-    </ThemeProvider>
+    <NavigationContainer>
+      <StackNavigator />
+      <StatusBar style="auto" />
+    </NavigationContainer>
   );
 }
 
--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -8,9 +8,6 @@ import BackupSettings from "../screens/settings/settings_backup";
 import ReaderSettings from "../screens/settings/settings_reader";
 import DownloadManager from "../screens/settings/storage_downloadmanager";
 
-
-
-
 const Stack = createNativeStackNavigator();
 
 export default function StackNavigator() {
@@ -21,11 +18,31 @@ export default function StackNavigator() {
         component={TabRoutes}
         options={{ headerShown: false }}
       />
-      <Stack.Screen name="GeneralSettings" component={GeneralSettings} />
-      <Stack.Screen name="ThemeSettings" component={ThemeSettings} />
-      <Stack.Screen name="BackupSettings" component={BackupSettings} />
-      <Stack.Screen name="DownloadManager" component={DownloadManager} />
-      <Stack.Screen name="ReaderSettings" component={ReaderSettings} />
+      <Stack.Screen
+        name="GeneralSettings"
+        component={GeneralSettings}
+        options={{ title: "General Settings" }}
+      />
+      <Stack.Screen
+        name="ThemeSettings"
+        component={ThemeSettings}
+        options={{ title: "Theme Settings" }}
+      />
+      <Stack.Screen
+        name="BackupSettings"
+        component={BackupSettings}
+        options={{ title: "Backup Settings" }}
+      />
+      <Stack.Screen
+        name="DownloadManager"
+        component={DownloadManager}
+        options={{ title: "Download Manager" }}
+      />
+      <Stack.Screen
+        name="ReaderSettings"
+        component={ReaderSettings}
+        options={{ title: "Reader Settings" }}
+      />
     </Stack.Navigator>
   );
 }
